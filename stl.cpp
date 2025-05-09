9664857bc69823f5de7e29b4d749e79bb73d9df3
Kaushik Kamarasu <kamarasukaushik9@gmail.com>
2025-05-09 16:13:59 +0530

    added stl.cpp

---
+++ stl.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define vll vector<ll>
+#define fore(i,n) for(ll i=0;i<n;i++)
+
+int main(){
+    vll v;
+    fore(i,n){
+        cin>>v[i];
+    }
+    cout<<v;
+}
