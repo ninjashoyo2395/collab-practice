ec43e19c53f583945aaba4390836407a1979651e
ninjashoyo2395 <cowshik78@gmail.com>
2025-05-09 16:19:03 +0530

    Update stl.cpp
    added comments to the stl.cpp file

--- stl.cpp
+++ stl.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #define ll long long int
 #define vll vector<ll>
 #define fore(i,n) for(ll i=0;i<n;i++)
-
+//bro this code is mid 
+//from maintaner
 int main(){
     vll v;
     fore(i,n){
